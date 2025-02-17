e6579df37b768d61cca279c179b38956380d4cc1
Siddharam <siddharamsutar23@gmail.com>
2025-02-17 11:55:25 +0530

    s

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
-    preview: {
-      allowedHosts: ['alfredtask-3.onrender.com']
-    }
+    // preview: {
+    //   allowedHosts: ['alfredtask-3.onrender.com']
+    // }
   }
 });
