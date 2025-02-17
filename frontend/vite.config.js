15b02082603d8f9acdfe1bf31e65cfe541e7ceaa
Siddharam <siddharamsutar23@gmail.com>
2025-02-17 12:27:25 +0530

    s

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(),react()],
+  plugins: [tailwindcss(), react()],
+  preview: {
+    host: '0.0.0.0', // Make the server accessible externally
+    port: 4173, // You can specify a port here, just like the local one
+  },
 })
